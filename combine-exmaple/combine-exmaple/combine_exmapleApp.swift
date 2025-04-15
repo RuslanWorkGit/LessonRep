31a2cc2d35876fd8bd87c31c82c629eb8f760149
RuslanWorkGit <lulka758@gmail.com>
2025-04-15 13:32:06 +0300

    Create empty combine project

---
+++ combine-exmaple/combine-exmaple/combine_exmapleApp.swift
@@ -0,0 +1,17 @@
+//
+//  combine_exmapleApp.swift
+//  combine-exmaple
+//
+//  Created by Ruslan Liulka on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct combine_exmapleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
