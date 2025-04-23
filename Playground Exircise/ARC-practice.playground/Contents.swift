fb25738805b71043a4a6d9efd4ada233533d3be3
RuslanWorkGit <lulka758@gmail.com>
2025-04-23 23:07:47 +0300

    Done task with club and members

--- Playground Exircise/ARC-practice.playground/Contents.swift
+++ Playground Exircise/ARC-practice.playground/Contents.swift
@@ -1,41 +1,106 @@
 import UIKit
 
-class Owner {
+//class Owner {
+//    var name: String
+//    var pet: Pet?
+//    
+//    init(name: String) {
+//        self.name = name
+//    }
+//    
+//    deinit {
+//        print("Owner \(name) deallocated")
+//    }
+//}
+//
+//class Pet {
+//    var name: String
+//    weak var owner: Owner?
+//    
+//    init(name: String) {
+//        self.name = name
+//    }
+//    
+//    deinit {
+//        print("Pet \(name) deallocated")
+//    }
+//}
+//
+//func testDeinit() {
+//    var o: Owner? = Owner(name: "Ruslan")
+//    var p: Pet? = Pet(name: "Sobachka")
+//    
+//    o?.pet = p
+//    p?.owner = o
+//    
+//    o = nil
+//    p = nil
+//}
+//
+//testDeinit()
+//print("Done")
+
+
+class Club {
     var name: String
-    var pet: Pet?
+    var members: [Member] = []
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func disBand() {
+        for member in members {
+            member.onTheLeftClub?()
+        }
+        members.removeAll()
+
+    }
+    
+    deinit {
+        print("Club \(name) deallocated")
+    }
+}
+
+class Member {
+    var name: String
+    weak var club: Club?
+    
+    var onTheLeftClub: (() -> Void)?
     
     init(name: String) {
         self.name = name
     }
     
     deinit {
-        print("Owner \(name) deallocated")
+        print("Member \(name) deallocated")
     }
 }
 
-class Pet {
-    var name: String
-    weak var owner: Owner?
+print("Start")
+func testClubDeinit() {
+    var club: Club? = Club(name: "NY-Club")
     
-    init(name: String) {
-        self.name = name
+    var member1: Member = Member(name: "Ruslan")
+    var member2: Member = Member(name: "Yana")
+    
+    club?.members = [member1, member2]
+    member1.club = club
+    member2.club = club
+    
+    member1.onTheLeftClub = {
+        print("Member1 deleted from club")
     }
     
-    deinit {
-        print("Pet \(name) deallocated")
+    member2.onTheLeftClub = {
+        print("Member2 deleted from club")
     }
+    
+    club?.disBand()
+    club = nil
+    
 }
 
-func testDeinit() {
-    var o: Owner? = Owner(name: "Ruslan")
-    var p: Pet? = Pet(name: "Sobachka")
-    
-    o?.pet = p
-    p?.owner = o
-    
-    o = nil
-    p = nil
-}
 
-testDeinit()
-print("Done")
+testClubDeinit()
+print("End")
