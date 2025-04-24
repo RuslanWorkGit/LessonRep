7a2b289e4b359c26d597e1e3bde810c93bb77406
RuslanWorkGit <lulka758@gmail.com>
2025-04-24 10:01:59 +0300

    add one more task

--- Playground Exircise/ARC-practice.playground/Contents.swift
+++ Playground Exircise/ARC-practice.playground/Contents.swift
@@ -41,66 +41,119 @@ import UIKit
 //print("Done")
 
 
-class Club {
-    var name: String
-    var members: [Member] = []
-    
-    init(name: String) {
-        self.name = name
-    }
-    
-    func disBand() {
-        for member in members {
-            member.onTheLeftClub?()
-        }
-        members.removeAll()
+//class Club {
+//    var name: String
+//    var members: [Member] = []
+//    
+//    init(name: String) {
+//        self.name = name
+//    }
+//    
+//    func disBand() {
+//        for member in members {
+//            member.onTheLeftClub?()
+//        }
+//        members.removeAll()
+//
+//    }
+//    
+//    deinit {
+//        print("Club \(name) deallocated")
+//    }
+//}
+//
+//class Member {
+//    var name: String
+//    weak var club: Club?
+//    
+//    var onTheLeftClub: (() -> Void)?
+//    
+//    init(name: String) {
+//        self.name = name
+//    }
+//    
+//    deinit {
+//        print("Member \(name) deallocated")
+//    }
+//}
+//
+//print("Start")
+//func testClubDeinit() {
+//    var club: Club? = Club(name: "NY-Club")
+//    
+//    var member1: Member = Member(name: "Ruslan")
+//    var member2: Member = Member(name: "Yana")
+//    
+//    club?.members = [member1, member2]
+//    member1.club = club
+//    member2.club = club
+//    
+//    member1.onTheLeftClub = {
+//        print("Member1 deleted from club")
+//    }
+//    
+//    member2.onTheLeftClub = {
+//        print("Member2 deleted from club")
+//    }
+//    
+//    club?.disBand()
+//    club = nil
+//    
+//}
+//
+//
+//testClubDeinit()
+//print("End")
 
-    }
-    
-    deinit {
-        print("Club \(name) deallocated")
-    }
-}
+//@MainActor
+//class DownloadManager {
+//    var task: DownloadTask?
+//    
+//    func startDownload() {
+//        let task = DownloadTask()
+//        
+//        self.task = task
+//        
+//        Task { [weak self] in
+//            guard let self = self else { return }
+//            
+//            await task.start()
+//            print("Comletion handler")
+//            
+//            self.task = nil
+//         }
+//    
+//    }
+//    
+//    deinit {
+//        print("Download manager deallocated")
+//    }
+//}
+//
+//class DownloadTask {
+//    func start() async {
+//        try? await Task.sleep(for: .seconds(2))
+//        print("Download finished")
+//    }
+//    
+//    deinit {
+//        print("DownloadTask deallocated")
+//    }
+//}
+//
+//print("Start")
+//
+//@MainActor
+//func test() async {
+//    var manager: DownloadManager? = DownloadManager()
+//    manager?.startDownload()
+//    
+//    try? await Task.sleep(for: .seconds(3))
+//    manager = nil
+//}
+//
+//Task {
+//    await test()
+//    print("END")
+//}
 
-class Member {
-    var name: String
-    weak var club: Club?
-    
-    var onTheLeftClub: (() -> Void)?
-    
-    init(name: String) {
-        self.name = name
-    }
-    
-    deinit {
-        print("Member \(name) deallocated")
-    }
-}
-
-print("Start")
-func testClubDeinit() {
-    var club: Club? = Club(name: "NY-Club")
-    
-    var member1: Member = Member(name: "Ruslan")
-    var member2: Member = Member(name: "Yana")
-    
-    club?.members = [member1, member2]
-    member1.club = club
-    member2.club = club
-    
-    member1.onTheLeftClub = {
-        print("Member1 deleted from club")
-    }
-    
-    member2.onTheLeftClub = {
-        print("Member2 deleted from club")
-    }
-    
-    club?.disBand()
-    club = nil
-    
-}
-
-
-testClubDeinit()
-print("End")
