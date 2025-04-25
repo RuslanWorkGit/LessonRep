140914b776b1a1c54cb65b7c501d3fc3c5933b9f
RuslanWorkGit <lulka758@gmail.com>
2025-04-25 10:52:51 +0300

    Create empty project

---
+++ Clean Swift Test/Clean Swift Test/CitiesPresenter.swift
@@ -0,0 +1,7 @@
+//
+//  CitiesPresenter.swift
+//  Clean Swift Test
+//
+//  Created by Ruslan Liulka on 25.04.2025.
+//
+
---
+++ Clean Swift Test/Clean Swift Test/CitiesWorker.swift
@@ -0,0 +1,8 @@
+
+//
+//  CittiesWorker.swift
+//  Clean Swift Test
+//
+//  Created by Ruslan Liulka on 25.04.2025.
+//
+
