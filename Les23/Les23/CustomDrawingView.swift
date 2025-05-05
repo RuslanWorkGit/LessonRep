8a7c99c1acb75a8d43459d5bdbb45a6fae8f33c6
RuslanWorkGit <lulka758@gmail.com>
2025-05-05 19:03:12 +0300

    add line

--- Les23/Les23/CustomDrawingView.swift
+++ Les23/Les23/CustomDrawingView.swift
@@ -5,3 +5,24 @@
 //  Created by Ruslan Liulka on 05.05.2025.
 //
 
+import UIKit
+
+class CustomDrawingView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        //Line
+        context.setStrokeColor(UIColor.purple.cgColor)
+        context.setFillColor(UIColor.orange.cgColor)
+        context.setLineWidth(3.0)
+        
+        context.move(to: CGPoint(x: 20, y: 20))
+        context.addLine(to: CGPoint(x: rect.width - 20, y: 20))
+        context.addLine(to: CGPoint(x: rect.width - 20, y: 60))
+        context.closePath()
+        //context.strokePath()
+        context.drawPath(using: .fillStroke)
+    }
+}
--- Les23/Les23/ViewController.swift
+++ Les23/Les23/ViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var circleView: CircleView!
+    @IBOutlet weak var customDrawingView: CustomDrawingView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        circleView.backgroundColor = .gray
+        customDrawingView.backgroundColor = .gray
     }
 
 
