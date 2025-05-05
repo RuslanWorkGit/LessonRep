19c288c55993ca5fcff963249d9f53e29b749a5a
RuslanWorkGit <lulka758@gmail.com>
2025-05-05 14:57:19 +0300

    Add les 23 empty proj

---
+++ Les23/Les23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Les23
+//
+//  Created by Ruslan Liulka on 05.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
